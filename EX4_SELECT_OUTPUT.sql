2e7ebd46c7592993bc32fb2191e863e5f3cc2b94
git4anirudha <64259508+git4anirudha@users.noreply.github.com>
2020-05-01 13:20:44 +0530

    Update EX4_SELECT_OUTPUT.sql

--- EX4_SELECT_OUTPUT.sql
+++ EX4_SELECT_OUTPUT.sql
@@ -1,5 +1,16 @@
-SET LINESIZE 1000;
-SET PAGESIZE 100;
+SET WRAP OFF;
+
+SET LINESIZE 400;
+SET PAGESIZE 50;
+
+column HEALTHPLAN format a12;
+column TABLE format a5;
+column COLUMNNAME format a20;
+column TEST_SCENARIO format a30;
+column SOURCE_COUNT format a12;
+column TARGET_COUNT format a12;
+column STATUS_DESC format a150;
+column DQ_REMARKS format a150;
 
 SPOOL C:\Users\aniruddhar\Desktop\SP_DQ_TRASH\Out.csv;
 
